62ccc0335cecbec1601d9bd0817a6342cb3401a3
Joe Molina <joedodaniljr123@gmail.com>
2024-03-08 12:44:15 -0400

    link en shy data y corregido max h mal puesto de main

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -13,7 +13,7 @@ async function Heeader() {
   return (
     <section className='z-20 h-16 flex items-center justify-between border-b bg-white  border-gray-200 fixed top-0 left-0 w-full'>
       <div className='w-52 h-10 flex items-center justify-center'>
-        <p className='text-black font-extrabold'>SyH Data</p>
+        <Link className='text-black font-extrabold' href='/'>SyH Data</Link>
       </div>
       {session? <span className='px-5 py-1 bg-zinc-900 rounded-sm text-white '>Usuario: {session.user?.name}</span> : <div></div>}
       {session? <ButtonSignOut/> : ''}
